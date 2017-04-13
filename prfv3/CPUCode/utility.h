49a8bcabcaa17701c684b92b77e3b729752ad69e
Giulio Stramondo <giuliostramondo@gmail.com>
2017-04-13 13:24:07 +0200

    Cleaned and added Lincense

--- prfv3/CPUCode/utility.h
+++ prfv3/CPUCode/utility.h
@@ -1,13 +1,20 @@
-//  
-//!  \file prf.h
-//  PRF_Sim
-//  
-//!  \file utility.h
-//  Utility
-//
-//  Created by Giulio Stramondo on 10/05/16.
-//  Copyright © 2016 UVA. All rights reserved.
-//
+/*
+    Copyright 2016 Giulio Stramondo
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #ifndef UTILITY_H
 #define UTILITY_H
--- prfv3/TESTS/README.md
+++
@@ -1,1 +0,0 @@
-The test code should reside in this folder instead of being bundled with the main program. 
